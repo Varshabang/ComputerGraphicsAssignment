4dc2bc783f5efefeebe301ffd81ef9dd7b43ccdb
Varshabang <57285793+Varshabang@users.noreply.github.com>
2021-08-03 13:09:51 +0530

    Add files via upload

---
+++ main - Copy.cpp
@@ -0,0 +1,37 @@
+/*
+ * GLUT Shapes Demo
+ *
+ * Written by Nigel Stewart November 2003
+ *
+ * This program is test harness for the sphere, cone
+ * and torus shapes in GLUT.
+ *
+ * Spinning wireframe and smooth shaded shapes are
+ * displayed until the ESC or q key is pressed.  The
+ * number of geometry stacks and slices can be adjusted
+ * using the + and - keys.
+ */
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+
+
+static void resize(int width, int height)
+{
+
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+   
+    glutCreateWindow("GLUT Shapes");
+
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    }
